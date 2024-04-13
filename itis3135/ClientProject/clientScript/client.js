69c9b35b7234a79ff0f733edaec7553cf458c050
Ben Cariello <bcariell@uncc.edu>
2024-04-13 16:29:13 -0400

    Client Project

---
+++ itis3135/ClientProject/clientScript/client.js
@@ -0,0 +1,27 @@
+
+//Nav scripts
+//Declaring varaibles
+const navButton = document.querySelector('#navButton');
+const dropdown = document.querySelector('#dropdownDiv');
+const dropDownElements = document.querySelectorAll('.dropdown');
+var keyArray = Object.keys(dropDownElements);
+
+
+navButton.addEventListener("mouseover", function() {
+    keyArray.forEach(function(key){
+        dropDownElements[key].style.display = 'block';
+    });
+});
+
+dropdown.addEventListener("mouseout", function (event) {
+    if (!dropdown.contains(event.relatedTarget)) {
+        keyArray.forEach(function(key){
+            dropDownElements[key].style.display = 'none';
+        });
+    }
+});
+
+
+
+
+
