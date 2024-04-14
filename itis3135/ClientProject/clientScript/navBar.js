2266178a540c1da3cfc51ce4f5e7ee6c51c34098
Ben Cariello <bcariell@uncc.edu>
2024-04-14 14:35:04 -0400

    Updates to Client Webpage

--- itis3135/ClientProject/clientScript/navBar.js
+++ itis3135/ClientProject/clientScript/navBar.js
@@ -6,13 +6,13 @@ const dropdown = document.querySelector('#dropdownDiv');
 const dropDownElements = document.querySelectorAll('.dropdown');
 var keyArray = Object.keys(dropDownElements);
 
-
+//Checks if the mouse is over the nav bar
 navButton.addEventListener("mouseover", function() {
     keyArray.forEach(function(key){
         dropDownElements[key].style.display = 'block';
     });
 });
-
+//checks if the mouse has left the nav bar
 dropdown.addEventListener("mouseout", function (event) {
     if (!dropdown.contains(event.relatedTarget)) {
         keyArray.forEach(function(key){
@@ -20,9 +20,3 @@ dropdown.addEventListener("mouseout", function (event) {
         });
     }
 });
-
-
-
-
-
-
---
+++ itis3135/ClientProject/clientScript/orderOnline.js
@@ -0,0 +1,96 @@
+//Online Order Scripts
+//Declaring Variables
+ var checkboxes = document.querySelectorAll('.orderCheck');
+ var amountInput = document.querySelectorAll('.amount');
+ var resetButton = document.getElementById('orderReset');
+ var submitButton = document.getElementById('orderSubmit');
+  let freshCost = 2.89;
+  let latteCost = 4.75; 
+  let blendedCost = 5.95;
+  let deluxeCost = 6.25;
+  let blueberryCost = 6.20;
+  let appleCost = 4.30;
+
+
+  
+  function calculateTotalPrice() {
+    var totalCost = 0;
+    // Loop through each checkbox
+    checkboxes.forEach(function(checkbox) {
+        if (checkbox.checked) {
+            // Get the corresponding amount input
+            var amountInput = document.getElementById(checkbox.id + 'Amount');
+            var quantity = parseInt(amountInput.value); // Get the quantity from the amount input
+            
+            // Calculate price based on checkbox and quantity
+            switch (checkbox.id) {
+                case 'fresh':
+                    totalCost += freshCost * quantity;
+                    break;
+                case 'latte':
+                    totalCost += latteCost * quantity;
+                    break;
+                case 'blended':
+                    totalCost += blendedCost * quantity;
+                    break;
+                case 'deluxe':
+                    totalCost += deluxeCost * quantity;
+                    break;
+                case 'blueberry':
+                    totalCost += blueberryCost * quantity;
+                    break;
+                case 'apple':
+                    totalCost += appleCost * quantity;
+                    break;
+            }
+        }
+    });
+     // Display total
+ document.getElementById('total').textContent = "Your total is: $" + totalCost.toFixed(2);
+  }
+
+ // Loop through each checkbox
+ checkboxes.forEach(function(checkbox) {
+     // Checks if a checkbox is selected
+     checkbox.addEventListener('change', function() {
+         // Get the corresponding number input field using the checkbox's id
+         var numberInput = document.getElementById(this.id + 'Amount');
+         var label = document.getElementById(this.id + 'Label');
+
+         // Toggle visibility of the number input based on checkbox's checked state
+         numberInput.style.display = this.checked ? 'inline-block' : 'none';
+         numberInput.required = this.checked ? 'true' : 'false';
+         label.style.display = this.checked ? 'inline-block' : 'none';
+         calculateTotalPrice();
+     });
+
+ });
+
+//Keeps the total up to date
+amountInput.forEach(function(checkInput) {
+    checkInput.addEventListener('change', function() {
+        calculateTotalPrice();
+    });
+});
+
+//Makes the reset button clear everything
+resetButton.addEventListener('click', function() {
+    //loop through every checkbox
+    checkboxes.forEach(function(checkbox) {
+        var numberInput = document.getElementById(checkbox.id + 'Amount');
+        var label = document.getElementById(checkbox.id + 'Label');
+        // Toggle visibility of the number input based on checkbox's checked state
+        numberInput.style.display ='none';
+        numberInput.required ='false';
+        label.style.display ='none';
+    });
+    
+    document.getElementById('total').textContent = "Your total is: $0.00";
+});
+
+//Displays information after food has been submitted
+submitButton.addEventListener('submit', function() {
+    let finalLabel = document.getElementById('#placedLabel')
+    finalLabel.style.display = 'inline-block';
+});
+ 
